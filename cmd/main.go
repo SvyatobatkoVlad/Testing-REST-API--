b10a1fa945995975a8f43c02e0d08d94aee4cc61
Vladyslav Sviatobatko <vladyslav.sviatobatko@technorely.com>
2021-03-31 21:55:37 +0300

    added model user and todo. Added handlers

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8000"); err != nil {
 		log.Fatal("error running hsttp server")
---
+++ pkg/handler/handler.go
@@ -0,0 +1,39 @@
+package handlre
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sing-up")
+		auth.POST("/sing-in")
+	}
+
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/")
+			lists.GET("/")
+			lists.GET("/:id")
+			lists.PUT("/:id")
+			lists.DELETE("/:id")
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/")
+				items.GET("/")
+				items.GET("/:items_id")
+				items.PUT("/:items_id")
+				items.DELETE("/:items_id")
+			}
+		}
+
+	}
+
+	return router
+}
