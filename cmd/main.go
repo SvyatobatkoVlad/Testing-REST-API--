b1f5a849e2c4624e7172c93b124ad30332d46447
Vladyslav Sviatobatko <vladyslav.sviatobatko@technorely.com>
2021-04-01 14:11:36 +0300

    added clean architecture

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"log"
+
+	todo "github.com/SvyatobatkoVlad/REST-API"
+	handler "github.com/SvyatobatkoVlad/REST-API/pkg/handler"
+	"github.com/SvyatobatkoVlad/REST-API/pkg/repository"
+	"github.com/SvyatobatkoVlad/REST-API/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatal("error running hsttp server")
 	}
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
-module github.com/SvyatobatkoVlad/REST-API.git
+module github.com/SvyatobatkoVlad/REST-API
 
 go 1.15
 
 require (
-	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
